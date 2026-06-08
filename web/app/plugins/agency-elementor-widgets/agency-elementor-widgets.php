d6e2806331b86b27819fff26ab2c1ad6047aa1c8
Nikola Chuchukovski <nikolacucukovski123@gmail.com>
2026-06-08 21:56:44 +0200

    icon-grid-v2: use !important on side-mode cards-box reset to beat the cards_box_padding/radius control's Elementor-wrapper selector (0,4,0) so cards don't double-pad inside the inner box

--- web/app/plugins/agency-elementor-widgets/agency-elementor-widgets.php
+++ web/app/plugins/agency-elementor-widgets/agency-elementor-widgets.php
@@ -13,7 +13,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'AEW_VERSION', '1.16.2' );
+define( 'AEW_VERSION', '1.16.3' );
 define( 'AEW_PLUGIN_FILE', __FILE__ );
 define( 'AEW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'AEW_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
