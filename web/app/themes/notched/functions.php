f20a4b208675d7265e856f8cb519e22169c126d9
Nikola Chuchukovski <nikolacucukovski123@gmail.com>
2026-06-09 10:32:27 +0200

    product pages: split button styling (Add-to-Cart/Get-a-Quote) into its own woo-buttons.css, separate from the variant-option styles in woo-variations.css

--- web/app/themes/notched/functions.php
+++ web/app/themes/notched/functions.php
@@ -34,7 +34,9 @@ add_action('wp_enqueue_scripts', function () {
         $uri = get_stylesheet_directory_uri();
         $cssv = is_readable("$dir/assets/woo-variations.css") ? (string) filemtime("$dir/assets/woo-variations.css") : null;
         $jsv  = is_readable("$dir/assets/woo-variations.js")  ? (string) filemtime("$dir/assets/woo-variations.js")  : null;
+        $btnv = is_readable("$dir/assets/woo-buttons.css")    ? (string) filemtime("$dir/assets/woo-buttons.css")    : null;
         wp_enqueue_style('notched-woo-variations', "$uri/assets/woo-variations.css", [], $cssv);
+        wp_enqueue_style('notched-woo-buttons', "$uri/assets/woo-buttons.css", [], $btnv);
         wp_enqueue_script('notched-woo-variations', "$uri/assets/woo-variations.js", ['jquery'], $jsv, true);
 
         // slug => #hex map for the colour attributes, read from term meta `swatch_hex`.
